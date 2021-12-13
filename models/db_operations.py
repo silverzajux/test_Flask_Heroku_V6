25ac38cf5254af767c3e699fa966f7b93c05a515
ntc_ahmettasci <atasci@natica.com.tr>
2021-12-13 15:04:26 +0300

    Heroku postgres database connection configured

--- models/db_operations.py
+++ models/db_operations.py
@@ -1,5 +1,14 @@
 import psycopg2
+import os
 
+# this version for Heroku database server
+def connect_to_db():
+    DATABASE_URL = os.environ['DATABASE_URL']
+    connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+    return connection
+
+''' This version for Local database server
 def connect_to_db():
     connection = psycopg2.connect(database='testDB',user='postgres',password='admin@local',host='127.0.0.1',port='5432')
     return connection
+'''
